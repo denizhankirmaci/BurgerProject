2afc9b38d7a43310d9fa403b5fd5a1342d5c2ffd
Denizhan <denizhankirmaci@gmail.com>
2024-12-22 03:01:49 +0300

    init

---
+++ src/components/MenuItem.jsx
@@ -0,0 +1,14 @@
+function MenuItem({ image, name, content, price }) {
+  return (
+    <div className="menuItem">
+      <div style={{ backgroundImage: `url(${image})` }}></div>
+      <h1 style={{ marginBottom: "10px" }}>{name}</h1>
+      <h6 style={{ marginTop: "0px" }}>{content}</h6>
+      <i style={{ color: "red" }}>
+        <p>{price} TL</p>
+      </i>
+    </div>
+  );
+}
+
+export default MenuItem;
---
+++ src/helpers/Data.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+import BurgerImage from "../assets/burger.jpg";
+
+export const Data = [
+  {
+    name: "Whooper",
+    image: BurgerImage,
+    content: "150 gr. kasap burger, patates, turşu",
+    price: 250,
+  },
+  {
+    name: "BigKing",
+    image: BurgerImage,
+    content: "150 gr. kasap burger, patates, turşu",
+    price: 250,
+  },
+  {
+    name: "Texas Smoke House",
+    image: BurgerImage,
+    content: "150 gr. kasap burger, patates, turşu",
+    price: 250,
+  },
+  {
+    name: "XXL",
+    image: BurgerImage,
+    content: "150 gr. kasap burger, patates, turşu",
+    price: 250,
+  },
+  {
+    name: "Big Mac",
+    image: BurgerImage,
+    content: "150 gr. kasap burger, patates, turşu",
+    price: 250,
+  },
+  {
+    name: "Hamburger",
+    image: BurgerImage,
+    content: "150 gr. kasap burger, patates, turşu",
+    price: 250,
+  },
+  {
+    name: "Cheeseburger",
+    image: BurgerImage,
+    content: "150 gr. kasap burger, patates, turşu",
+    price: 250,
+  },
+  {
+    name: "Italiano",
+    image: BurgerImage,
+    content: "150 gr. kasap burger, patates, turşu",
+    price: 250,
+  },
+];
